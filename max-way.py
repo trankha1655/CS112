6de53039cc549293a5e8ae8590d4376431b649c6
trankha1655 <phamtruong7a1@gmail.com>
2021-01-06 10:13:49 +0700

    Update max-way.py

--- max-way.py
+++ max-way.py
@@ -1,1 +1,19 @@
 
+
+
+m,n= map(int,input().split())
+
+L0= (n+1)*[0]
+
+
+L=[]
+for i in range(m):
+    L= list(map(int,input().split()))+[0]
+
+    for j in range(n):
+        if L[j]:
+            L0[j]= max(L0[j-1],L0[j]) +L[j]
+
+        else:
+            L0[j]=0
+print(L0[n-1])
